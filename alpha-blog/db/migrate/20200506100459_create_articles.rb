cb2cf2e0b4586092cc1ea1b09b7d39424841c0e5
aruchen <achen@bmgww.com>
2020-05-07 19:44:01 +0800

    0507

--- alpha-blog/db/migrate/20200506100459_create_articles.rb
+++ alpha-blog/db/migrate/20200506100459_create_articles.rb
@@ -1,6 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
+      # 建立一個名稱為articles的資料庫表格，然後裏面有以下的內容與資料格式
       t.string :title
       t.text :description
     end
